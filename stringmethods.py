de232e267b95de4cb5621cd4c9a8f55ea5df24c5
pawanpreetkaur-coder <preetpawan1819@gmail.com>
2025-07-07 11:42:22 +0530

    Add files via upload

---
+++ stringmethods.py
@@ -0,0 +1,86 @@
+def strings():
+    print("-----different string methods performed  using word *Hello coder*------ ")
+    a="Hello coder"
+    print("upper():",a.upper())
+    print("lower():",a.lower())
+    print("capitalize():",a.capitalize())
+    print("title():",a.title())
+    print("swapcase():",a.swapcase())
+    
+    print("----------------------------------------------")
+    print("hello coder islower():",'hello coder'.islower())
+    print("HELLO CODER islower():",'HELLO CODER'.islower())
+    
+    print("----------------------------------------------")
+    
+    print("hello coder isupper():",'hello coder'.isupper())
+    print("HELLO CODER isupper():",'HELLO CODER'.isupper())
+    
+    print("----------------------------------------------")
+    
+    print("Hello Coder istitle():",'Hello Coder'.istitle())
+    print("Hello coder istitle():",'Hello coder'.istitle())
+    
+    print("----------------------------------------------")
+    
+    print("hellocoder isalpha():",'hellocoder'.isalpha())
+    print("hello coder isalpha():",'hello coder'.isalpha())
+    
+    print("----------------------------------------------")
+    
+    print("hellocoder87 isalnum():",'hellocoder87'.isalnum())
+    print("hello coder87 isalnum():",'hello coder87'.isalnum())
+    
+    print("----------------------------------------------")
+    
+    print("12345 isdigit():",'12345'.isdigit())
+    print("hello coder isdigit():",'hello coder'.isdigit())
+    
+    print("----------------------------------------------")
+    
+    # print("h e l l o isspace():",'h e l l o'.isspace())
+    # print("hello coder isspace():",'hello coder'.isspace())
+    
+    print("----------------------------------------------------------")
+    
+    print("hello coder endswith(coder):",'hello coder'.endswith('coder'))
+    print("hello coder endswith(hello):",'hello coder'.endswith('hello'))
+    
+    print("-----------------------------------------------------------")
+    
+    print("hello coder startwith(coder):",'hello coder'.startswith('coder'))
+    print("hello coder startwith(hello):",'hello coder'.startswith('hello'))
+    
+    print("-----------------------------------------------------------")
+    
+    balls='red,green,red,blue,red'
+    print("count balls (red):",balls.count('red'))
+    
+    print("-----------------------------------------------------------")
+    
+    word='thank you'
+    print(" *thank you* find (n) :",word.find('k'))
+    
+    print("-----------------------------------------------------------")
+    
+    print("hellocoder strip():",'hellocoder'.strip('coder'))
+    print("hellocoder lstrip():",'hellocoder'.lstrip('coder'))
+    print("hellocoder rstrip():",'hellocoder'.rstrip('coder'))
+    
+    print("-----------------------------------------------------------")
+    
+    furniture='sofa bed table chair '
+    print(" split( sofa bed table chair):",furniture.split())
+    
+    print("-----------------------------------------------------------")
+    
+    
+    # print("hellocoder ljust():",'hello coder'.ljust())
+    
+    # print("-----------------------------------------------------------")
+    
+    # text='hello'
+    # print("replace():",text.replace('coder'))
+    
+    
+strings()    
