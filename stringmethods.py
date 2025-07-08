a6b789f930fe0ca0b532a157d74b7eb5ad781bc2
pawanpreetkaur-coder <preetpawan1819@gmail.com>
2025-07-08 11:52:51 +0530

    Add files via upload

--- stringmethods.py
+++ stringmethods.py
@@ -1,86 +1,113 @@
-def strings():
-    print("-----different string methods performed  using word *Hello coder*------ ")
-    a="Hello coder"
-    print("upper():",a.upper())
-    print("lower():",a.lower())
-    print("capitalize():",a.capitalize())
-    print("title():",a.title())
-    print("swapcase():",a.swapcase())
+print("-----different string methods performed  using word *Hello coder*------ ")
+def exupper():
+
+    print("upper():",'Hello coder'.upper())
+exupper()    
+
+def exlower():
+    print("lower():",'Hello coder'.lower())
+exlower()    
+
+def excapitalize():  
+    print("capitalize():",'Hello coder'.capitalize())
+excapitalize()    
     
-    print("----------------------------------------------")
+def extitle():    
+    print("title():",'Hello coder'.title())
+extitle()   
+
+def swapcase():
+    print("swapcase():",'Hello coder'.swapcase())
+swapcase()
+    
+print("----------------------------------------------")
+
+def exislower():
     print("hello coder islower():",'hello coder'.islower())
     print("HELLO CODER islower():",'HELLO CODER'.islower())
-    
-    print("----------------------------------------------")
-    
+exislower()    
+
+print("----------------------------------------------")
+
+def exisupper():    
     print("hello coder isupper():",'hello coder'.isupper())
     print("HELLO CODER isupper():",'HELLO CODER'.isupper())
-    
-    print("----------------------------------------------")
-    
+exisupper()  
+
+print("----------------------------------------------")
+ 
+def existitle():    
     print("Hello Coder istitle():",'Hello Coder'.istitle())
     print("Hello coder istitle():",'Hello coder'.istitle())
-    
-    print("----------------------------------------------")
-    
+existitle() 
+   
+print("----------------------------------------------")
+ 
+def exisalpha():   
     print("hellocoder isalpha():",'hellocoder'.isalpha())
     print("hello coder isalpha():",'hello coder'.isalpha())
-    
-    print("----------------------------------------------")
-    
+exisalpha()  
+
+print("----------------------------------------------")
+  
+def exisalnum():    
     print("hellocoder87 isalnum():",'hellocoder87'.isalnum())
     print("hello coder87 isalnum():",'hello coder87'.isalnum())
+exisalnum()
     
-    print("----------------------------------------------")
-    
+print("----------------------------------------------")
+
+def exisdigit():    
     print("12345 isdigit():",'12345'.isdigit())
     print("hello coder isdigit():",'hello coder'.isdigit())
+exisdigit() 
+   
+# print("----------------------------------------------")
+
+# def exisspace():   
+#     print("h e l l o isspace():",'h e l l o'.isspace())
+#     print("hello isspace():",'hello'.isspace())
+# exisspace()
     
-    print("----------------------------------------------")
-    
-    # print("h e l l o isspace():",'h e l l o'.isspace())
-    # print("hello coder isspace():",'hello coder'.isspace())
-    
-    print("----------------------------------------------------------")
-    
+print("----------------------------------------------------------")
+
+def exendswith():    
     print("hello coder endswith(coder):",'hello coder'.endswith('coder'))
     print("hello coder endswith(hello):",'hello coder'.endswith('hello'))
-    
-    print("-----------------------------------------------------------")
-    
+exendswith()  
+ 
+print("-----------------------------------------------------------")
+
+def exstartswith():    
     print("hello coder startwith(coder):",'hello coder'.startswith('coder'))
     print("hello coder startwith(hello):",'hello coder'.startswith('hello'))
-    
-    print("-----------------------------------------------------------")
-    
+exstartswith()  
+  
+print("-----------------------------------------------------------")
+
+def excount():   
     balls='red,green,red,blue,red'
     print("count balls (red):",balls.count('red'))
+excount()
     
-    print("-----------------------------------------------------------")
-    
+print("-----------------------------------------------------------")
+ 
+def exfind():    
     word='thank you'
     print(" *thank you* find (n) :",word.find('k'))
+exfind()   
+
+# print("-----------------------------------------------------------")
+
+# def Exstrip():    
+#     print(" hellocoder strip():",'        hellocoder '.strip())
     
-    print("-----------------------------------------------------------")
-    
-    print("hellocoder strip():",'hellocoder'.strip('coder'))
-    print("hellocoder lstrip():",'hellocoder'.lstrip('coder'))
-    print("hellocoder rstrip():",'hellocoder'.rstrip('coder'))
-    
-    print("-----------------------------------------------------------")
-    
-    furniture='sofa bed table chair '
-    print(" split( sofa bed table chair):",furniture.split())
-    
-    print("-----------------------------------------------------------")
-    
-    
-    # print("hellocoder ljust():",'hello coder'.ljust())
-    
-    # print("-----------------------------------------------------------")
-    
-    # text='hello'
-    # print("replace():",text.replace('coder'))
-    
-    
-strings()    
+# Exstrip()   
+
+print("-----------------------------------------------------------")
+ 
+def exsplit():
+    furniture='sofa bed table chair'
+    print("split(sofa bed table chair):",furniture.split())
+exsplit()    
+
